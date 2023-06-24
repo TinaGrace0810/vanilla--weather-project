77fe1b198e9a2b51edcdd3393dda3f69a3403d1d
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-06-24 18:12:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,8 +70,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "eaat2082d1dabe69502o3fa4aa574ecb";
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric;";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
